e145cbf08e49e82b4f0216966550f41acacd8939
ajsushant1 <sushantajgaonkar1766@gmail.com>
2020-02-15 16:45:14 +0530

    ADDED: If statement to check head or tail

--- FlipCoinSimulation.sh
+++ FlipCoinSimulation.sh
@@ -2,4 +2,18 @@
 
 echo "/******************************************* WELCOME TO FLIP COIN SIMULATION *******************************************/"
 
+# CONSTANT
+IS_HEAD=1
+
+# TO GET RANDOM VALUE
+randomCheck=$((RANDOM%2))
+
+# CHECKING HEAD OR TAIL AND DISPLAYING RESULT
+if [ $randomCheck -eq $IS_HEAD ]
+then
+	echo "Head"
+else
+	echo "Tail"
+fi
+
 
