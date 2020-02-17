0792f59b1812dfb8b8a8b355069d6b36a0e8c411
ajsushant1 <sushantajgaonkar1766@gmail.com>
2020-02-17 11:18:52 +0530

    ADDED: Random function and Computing percenatge of singlet combination

--- FlipCoinSimulation.sh
+++ FlipCoinSimulation.sh
@@ -5,15 +5,40 @@ echo "/******************************************* WELCOME TO FLIP COIN SIMULATI
 # CONSTANT
 IS_HEAD=1
 
-# TO GET RANDOM VALUE
-randomCheck=$((RANDOM%2))
+#VARIABLES
+headCount=0
+tailCount=0
 
-# CHECKING HEAD OR TAIL AND DISPLAYING RESULT
-if [ $randomCheck -eq $IS_HEAD ]
+# DECLARING DICTIONARY
+declare -A singletCombination
+
+# READING FLIPPING COUNT FROM USER
+read -p "Enter a number of time you want to flip a coin : " numberOfCoinFlip
+
+# TO GET RANDOM NUMBER
+function getRandom(){
+	randomCheck=$((RANDOM%2))
+	echo $randomCheck
+}
+
+# COMPUTING HEADCOUNT AND TAILCOUNT THEN STORING IN DICTIONARY
+if [ $numberOfCoinFlip -ne 0 ]
 then
-	echo "Head"
-else
-	echo "Tail"
-fi
+	for (( flip=1; flip<=numberOfCoinFlip; flip++ ))
+	{
+		result="$( getRandom )"
+		if [ $result -eq $IS_HEAD ]
+		then
+			singletCombination[head]=$((++headCount))
+		else
+			singletCombination[tail]=$((++tailCount))
+		fi
+	}
 
+# CALCULATING PERCENATGE OF SINGLET PERCENTAGE
+	headPercentage=`echo "scale=2; $headCount*100/$numberOfCoinFlip" | bc`
+	tailPercentage=`echo "scale=2; $tailCount*100/$numberOfCoinFlip" | bc`
+else
+	echo "Please enter the number greater then Zero"
+fi
 
